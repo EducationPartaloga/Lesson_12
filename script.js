e09c814f3eda5b8dd8d04df30aee62df9e2f5852
PartalogaIrina <7718118@gmail.com>
2023-03-04 14:01:48 +0200

    546

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-   let arr = [14, 8, 0, 23, 1, 135]
+//    let arr = [14, 8, 0, 23, 1, 135]
 
-   console.log(arr.sort((a, b) => { return a - b }));
+//    console.log(arr.sort((a, b) => { return a - b }));
+
+
+
