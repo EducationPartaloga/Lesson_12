9acc76e80e60bb1c6ae3c6aa831fd67ebc9fee52
PartalogaIrina <7718118@gmail.com>
2023-03-04 14:49:02 +0200

    452

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function rgb(r, g, b){
 
     if (r > 255) r = 255
     if (r < 0) r = 0
-    if (g > 255) r = 255
-    if (g < 0) r = 0
-    if (b > 255) r = 255
-    if (b < 0) r = 0
+    if (g > 255) g = 255
+    if (g < 0) g = 0
+    if (b > 255) b = 255
+    if (b < 0) b = 0
 
     let red, green, blue = ''
 
@@ -37,12 +37,7 @@ function rgb(r, g, b){
     }else{
         blue = b.toString(16)
     }
-
-
     
-
-
-
 
     return red + green + blue
     
