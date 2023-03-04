443b765698ab5bd01e06a53794fe3104f6349140
PartalogaIrina <7718118@gmail.com>
2023-03-04 14:33:34 +0200

    4564

--- script.js
+++ script.js
@@ -4,3 +4,26 @@
 
 
 
+function rgb(r, g, b){
+
+    // console.log(r.toString(16));
+    // console.log(g.toString(16));
+    // console.log(b.toString(16));
+
+    if (r > 255) r = 255
+    if (r < 0) r = 0
+    if (g > 255) r = 255
+    if (g < 0) r = 0
+    if (b > 255) r = 255
+    if (b < 0) r = 0
+
+    
+
+    return r.toString(16) + g.toString(16) + b.toString(16)
+    
+    
+}
+
+console.log(rgb(255, 100, 124));
+
+rgb (255, 0, 124)
